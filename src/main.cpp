e63f3c818151d6d400095ec2fd76c9d3a7856dd4
avikam <avikam@gmail.com>
2017-12-22 19:24:16 +0200

    adding ini parsing, thant to inih contributers

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
+#include <string>
+#include "src/external/inih-master/INIReader.h"
 #include "src/config.h"
 #include "src/game.h"
 
 namespace pong {
-    static config init_config(){
+    static config init_config(int argc, const char* const argv[]){
         config conf {};
+        if (argc > 1) {
+            std::string arg1 { argv[1] };
+
+            INIReader reader(arg1);
+
+            if (reader.ParseError() < 0) {
+                std::cout << "Can't load "<< arg1 << "\n";
+            }
+
+            conf.max_score = static_cast<int>(reader.GetInteger("config", "max_score", constants::max_score));
+        }
         // conf.max_seconds_idle = 2;
-        conf.max_score = 2;
 
         return conf;
     };
 }
 
-int main() {
-    auto conf = pong::init_config();
+int main(int argc, const char* const argv[]) {
+    auto conf = pong::init_config(argc, argv);
 
     pong::game g { conf };
     g.play();
